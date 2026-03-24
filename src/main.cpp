ad3a9bd81d12c1119769d9efe3172c8749ac9533
Abhay Manoj <melchior88729@gmail.com>
2026-03-24 19:05:49 -0400

    Changed to draw_star func

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 static constexpr int WIDTH = 1280;
 static constexpr int HEIGHT = 720;
+static constexpr uint32_t RED = 0xFFFF0000;
 static const std::string TITLE = "Abhay's Rasterizer";
 
 struct AppState {
@@ -25,6 +26,17 @@ struct AppState {
   SDL_Texture *texture = nullptr;
   std::unique_ptr<FrameBuffer> buffer;
 };
+void draw_star(FrameBuffer &buffer) {
+  Vec2 p1 = {640, 110};
+  Vec2 p2 = {423, 485};
+  Vec2 p3 = {857, 485};
+  Vec2 p4 = {640, 610};
+  Vec2 p5 = {423, 235};
+  Vec2 p6 = {857, 235};
+
+  draw_triangle(buffer, p1, p2, p3, 0xFFFF0000);
+  draw_triangle(buffer, p4, p5, p6, 0xFFFF0000);
+}
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -34,6 +46,8 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   auto *state = new AppState();
   state->buffer = std::make_unique<FrameBuffer>(WIDTH, HEIGHT);
   *appstate = state;
+
+  draw_star(*state->buffer);
 
   if (!SDL_CreateWindowAndRenderer(TITLE.c_str(), WIDTH, HEIGHT, 0,
                                    &state->window, &state->renderer)) {
