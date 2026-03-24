09963429a39aa22aff556e479a0713a89ef65128
Abhay Manoj <melchior88729@gmail.com>
2026-03-24 03:05:58 -0400

    Added out of bounds check, made setPixel take in a color, changed the name from clear to fil and also changed from std::size_t to size_t

--- src/frame_buffer.hpp
+++ src/frame_buffer.hpp
@@ -9,22 +9,25 @@
 
 // @brief contains the data for pixels on the screen
 struct FrameBuffer {
-  std::size_t width;
-  std::size_t height;
+  size_t width;
+  size_t height;
   std::vector<uint32_t> pixels;
 
   // @brief constructs a empty FrameBuffer
   // @param w the width of the screen
   // @param h the height of the screen
-  FrameBuffer(std::size_t w, std::size_t h)
+  FrameBuffer(size_t w, size_t h)
       : width(w), height(h), pixels(width * height) {}
 
-  void setPixel(std::size_t x, std::size_t y, std::uint8_t r, std::uint8_t g,
-                std::uint8_t b) {
-    pixels[y * width + x] = (0xFF << 24) | (r << 16) | (g << 8) | b;
+  void setPixel(int x, int y, uint32_t color) {
+    if (x < 0 || x >= (int)width || y < 0 || y >= (int)height) {
+      return;
+    }
+
+    pixels[y * width + x] = color;
   }
 
-  void clear(uint32_t color = 0xFF000000) {
+  void fill(uint32_t color = 0xFF000000) {
     std::fill(pixels.begin(), pixels.end(), color);
   }
 };
