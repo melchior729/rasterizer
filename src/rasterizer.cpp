5223b9e7e19ea796881157d4869a8fccfbc1209b
Abhay Manoj <melchior88729@gmail.com>
2026-03-25 04:32:06 -0400

    changed name of determinant to edge function

--- src/rasterizer.cpp
+++ src/rasterizer.cpp
@@ -43,32 +43,29 @@ void draw_triangle(FrameBuffer &buffer, Vec2 p0, Vec2 p1, Vec2 p2,
   draw_line(buffer, p1, p2, color);
 }
 
-static double get_determinant(Vec2 p0, Vec2 p1, Vec2 p2) {
+static double edge_function(Vec2 p0, Vec2 p1, Vec2 p2) {
   return (double)(p2.y * (p1.x - p0.x) - p0.y * (p1.x - p2.x) +
                   p1.y * (p0.x - p2.x));
 }
 
 void draw_filled_triangle(FrameBuffer &buffer, Vec2 p0, Vec2 p1, Vec2 p2,
                           uint32_t color) {
-  const double det = get_determinant(p0, p1, p2);
+  const double det = edge_function(p0, p1, p2);
   if (std::abs(det) < 1e-7) {
     return;
   }
-
   const double inv_det = 1.0 / det;
   const int min_x = std::min({p0.x, p1.x, p2.x});
   const int min_y = std::min({p0.y, p1.y, p2.y});
   const int max_x = std::max({p0.x, p1.x, p2.x});
   const int max_y = std::max({p0.y, p1.y, p2.y});
-
   for (int y = min_y; y <= max_y; y++) {
     for (int x = min_x; x <= max_x; x++) {
-      Vec2 p = {x, y};
-      double l1 = get_determinant(p, p1, p2) * inv_det;
-      double l2 = get_determinant(p0, p, p2) * inv_det;
-      double l3 = 1.0 - l1 - l2;
+      const double l1 = edge_function({x, y}, p1, p2) * inv_det;
+      const double l2 = edge_function(p0, {x, y}, p2) * inv_det;
+      const double l3 = 1.0 - l1 - l2;
       if (l1 >= 0 && l2 >= 0 && l3 >= 0) {
-        draw_point(buffer, p, color);
+        draw_point(buffer, {x, y}, color);
       }
     }
   }
