8d23c102b9b764260f8bcce5561056361bdccf7f
Abhay Manoj <melchior88729@gmail.com>
2026-05-19 20:25:44 -0400

    refactor: removed meshes from scene, it only renders one object now

--- include/scene.hpp
+++ include/scene.hpp
@@ -3,12 +3,12 @@
 #include "camera.hpp"
 #include "config.hpp"
 #include "frame_buffer.hpp"
+#include "model.hpp"
 
 struct SceneConfig {
   Vec3 light_dir;
-  Vec3 rot;
   RenderMode mode;
 };
 
-int draw_scene(FrameBuffer &buffer, const Camera &camera,
+int draw_scene(FrameBuffer &buffer, SceneObject &o, const Camera &camera,
                const SceneConfig &config);
--- src/scene.cpp
+++ src/scene.cpp
@@ -5,23 +5,6 @@
 static constexpr float ambient{0.2f};
 static Mat4 projection{project()};
 
-static Mesh cube_mesh{Mesh::load("cube.obj")};
-static Mesh cow_mesh{Mesh::load("cow.obj")};
-static Mesh man_mesh{Mesh::load("man.obj")};
-
-std::vector<SceneObject> get_objects(Vec3 r) {
-  std::vector<SceneObject> objects;
-  SceneObject cube{cube_mesh, {10, 0, -10}, {1.0f, 1.0f, 1.0f}, r};
-  SceneObject cow{cow_mesh, {-10, 0, -20}, {0.8f, 0.8f, 0.8f}, r};
-  SceneObject man{man_mesh, {0, -5, -10}, {0.4f, 0.4f, 0.4f}, r};
-
-  // objects.push_back(cube);
-  // objects.push_back(cow);
-  objects.push_back(man);
-  // objects.push_back(gun);
-  return objects;
-}
-
 static int get_visible_faces(const Camera &camera, std::vector<Face> &faces,
                              std::vector<Face> &visible_faces,
                              std::vector<Vertex> &vertices) {
@@ -95,24 +78,20 @@ static void draw_faces(FrameBuffer &buffer, const Vec3 light_dir,
   }
 }
 
-int draw_scene(FrameBuffer &buffer, const Camera &camera,
+int draw_scene(FrameBuffer &buffer, SceneObject &o, const Camera &camera,
                const SceneConfig &config) {
   int num_faces{};
-  auto objects{get_objects(config.rot)};
-  for (auto &o : objects) {
-    auto vertices{o.mesh.vertices};
-    for (auto &v : vertices) {
-      v.pos = camera.view() * o.model * v.pos;
-      v.normal = (camera.view() * o.model * v.normal).xyz();
-    }
-
-    std::vector<Face> visible_faces{};
-    num_faces +=
-        get_visible_faces(camera, o.mesh.faces, visible_faces, vertices);
-
-    perspective_divide_and_screen_space(vertices);
-    draw_faces(buffer, config.light_dir, visible_faces, vertices, config.mode);
+  auto vertices{o.mesh.vertices};
+  for (auto &v : vertices) {
+    v.pos = camera.view() * o.model * v.pos;
+    v.normal = (camera.view() * o.model * v.normal).xyz();
   }
+
+  std::vector<Face> visible_faces{};
+  num_faces += get_visible_faces(camera, o.mesh.faces, visible_faces, vertices);
+
+  perspective_divide_and_screen_space(vertices);
+  draw_faces(buffer, config.light_dir, visible_faces, vertices, config.mode);
 
   return num_faces;
 }
