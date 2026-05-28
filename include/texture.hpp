d4bd8bd9f94a5540e5217985d64f9b82c038c544
Abhay Manoj <sanicthecheese@gmail.com>
2026-05-28 15:54:45 -0400

    refactor: moved texture code to .cpp file

--- include/texture.hpp
+++ include/texture.hpp
@@ -1,8 +1,6 @@
 #include "color.hpp"
 #include "stb_image.h"
-#include <algorithm>
 #include <cassert>
-#include <cstdint>
 #include <string>
 #include <vector>
 
@@ -11,35 +9,7 @@ struct Texture {
   int width{};
   int height{};
 
-  Color sample(float u, float v) {
-    float fx = u * static_cast<float>(width - 1);
-    float fy = v * static_cast<float>(height - 1);
+  Color sample(float u, float v);
 
-    auto x = std::clamp(static_cast<std::size_t>(fx), std::size_t{0},
-                        static_cast<std::size_t>(width - 1));
-    auto y = std::clamp(static_cast<std::size_t>(fy), std::size_t{0},
-                        static_cast<std::size_t>(height - 1));
-
-    return pixels[y * static_cast<size_t>(width) + x];
-  }
-
-  static Texture load(const std::string &path) {
-    Texture t;
-    int channels;
-    uint8_t *data = stbi_load(path.c_str(), &t.width, &t.height, &channels, 4);
-    assert(data);
-
-    auto n = static_cast<std::size_t>(t.width * t.height);
-    t.pixels.resize(n);
-    for (std::size_t i = 0; i < n; i++) {
-      uint32_t r = data[i * 4 + 0];
-      uint32_t g = data[i * 4 + 1];
-      uint32_t b = data[i * 4 + 2];
-      uint32_t a = data[i * 4 + 3];
-      t.pixels[i] = {a, r, g, b};
-    }
-
-    stbi_image_free(data);
-    return t;
-  }
+  static Texture load(const std::string &path);
 };
