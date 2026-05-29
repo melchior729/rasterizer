0478d605f5e521d30f6af1229ebdb9530b08f603
Abhay Manoj <melchior88729@gmail.com>
2026-05-29 01:47:06 -0400

    refactor: split triangle() into different functions

--- src/rasterizer.cpp
+++ src/rasterizer.cpp
@@ -4,13 +4,16 @@
 
 static constexpr float epsilon{-1e-4f};
 
+struct Bounds {
+  int min_x;
+  int min_y;
+  int max_x;
+  int max_y;
+};
+
 void point(FrameBuffer &buffer, const Vertex &p, const Color color) {
   buffer.set(static_cast<int>(p.pos.x), static_cast<int>(p.pos.y), p.pos.z,
              color);
-}
-
-static float det(const Vec4 &a, const Vec4 &b, const Vec4 &c) {
-  return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y);
 }
 
 void line(FrameBuffer &buffer, const Vertex &a, const Vertex &b) {
@@ -48,6 +51,36 @@ void line(FrameBuffer &buffer, const Vertex &a, const Vertex &b) {
   }
 }
 
+static Bounds calculate_bounds(const Vertex &a, const Vertex &b,
+                               const Vertex &c) {
+  return {
+      std::clamp(
+          static_cast<int>(std::floor(std::min({a.pos.x, b.pos.x, c.pos.x}))),
+          0, WIDTH),
+      std::clamp(
+          static_cast<int>(std::floor(std::min({a.pos.y, b.pos.y, c.pos.y}))),
+          0, HEIGHT),
+      std::clamp(
+          static_cast<int>(std::ceil(std::max({a.pos.x, b.pos.x, c.pos.x}))), 0,
+          WIDTH),
+      std::clamp(
+          static_cast<int>(std::ceil(std::max({a.pos.y, b.pos.y, c.pos.y}))), 0,
+          HEIGHT)};
+}
+
+static float det(const Vec2 &a, const Vec2 &b, const Vec2 &c) {
+  return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y);
+}
+
+static bool get_barycentric(const Vec4 &p, const Vec4 &a, const Vec4 &b,
+                            const Vec4 &c, const float inv_det, float &l1,
+                            float &l2, float &l3) {
+  l1 = det({p.x, p.y}, {b.x, b.y}, {c.x, c.y}) * inv_det;
+  l2 = det({a.x, a.y}, {p.x, p.y}, {c.x, c.y}) * inv_det;
+  l3 = det({a.x, a.y}, {b.x, b.y}, {p.x, p.y}) * inv_det;
+  return l1 >= epsilon && l2 >= epsilon && l3 >= epsilon;
+}
+
 void triangle_wireframe(FrameBuffer &buffer, const Vertex &a, const Vertex &b,
                         const Vertex &c) {
   line(buffer, a, b);
@@ -55,10 +88,72 @@ void triangle_wireframe(FrameBuffer &buffer, const Vertex &a, const Vertex &b,
   line(buffer, b, c);
 }
 
+static float interpolate(const float a, const float b, const float c,
+                         const float l1, const float l2, const float l3) {
+  return a * l1 + b * l2 + c * l3;
+}
+
+uint32_t compute_channel(float base, float spec, float brightness) {
+  float shaded{AMBIENT + (brightness * base * CHANNEL_VAL_F) + spec};
+  return std::clamp(static_cast<uint32_t>(shaded), 0U, 255U);
+}
+
+static Color shade_pixel(const Vertex &a, const Vertex &b, const Vertex &c,
+                         const Vec3 light_dir, const float l1, const float l2,
+                         const float l3, const float bright_a,
+                         const float bright_b, const float bright_c,
+                         const Material &material, RenderMode mode) {
+
+  Vec3 normal{
+      norm({interpolate(a.normal.x, b.normal.x, c.normal.x, l1, l2, l3),
+            interpolate(a.normal.y, b.normal.y, c.normal.y, l1, l2, l3),
+            interpolate(a.normal.z, b.normal.z, c.normal.z, l1, l2, l3)})};
+
+  auto brightness{interpolate(bright_a, bright_b, bright_c, l1, l2, l3)};
+  float spec_r{}, spec_g{}, spec_b{};
+
+  if (mode == RenderMode::Phong) {
+    Vec3 view_pos{norm({
+        -interpolate(a.view_pos.x, b.view_pos.x, c.view_pos.x, l1, l2, l3),
+        -interpolate(a.view_pos.y, b.view_pos.y, c.view_pos.y, l1, l2, l3),
+        -interpolate(a.view_pos.z, b.view_pos.z, c.view_pos.z, l1, l2, l3),
+    })};
+
+    auto H{norm(light_dir + view_pos)};
+    auto spec{std::pow(std::max(normal.dot(H), 0.0f), material.shine)};
+
+    spec_r = material.specular.r() * spec;
+    spec_g = material.specular.g() * spec;
+    spec_b = material.specular.b() * spec;
+
+    brightness = std::clamp(normal.dot(light_dir), AMBIENT, 1.0f);
+  }
+
+  float r_ch = material.diffuse.r();
+  float g_ch = material.diffuse.g();
+  float b_ch = material.diffuse.b();
+
+  if (material.texture && !material.texture->pixels.empty()) {
+    auto u{interpolate(a.uv.x, b.uv.x, c.uv.x, l1, l2, l3)};
+    auto v{interpolate(a.uv.y, b.uv.y, c.uv.y, l1, l2, l3)};
+    auto texel{material.texture->sample(u, v)};
+
+    r_ch = texel.r();
+    g_ch = texel.g();
+    b_ch = texel.b();
+  }
+
+  auto R{compute_channel(r_ch / CHANNEL_VAL_F, spec_r, brightness)};
+  auto G{compute_channel(g_ch / CHANNEL_VAL_F, spec_g, brightness)};
+  auto B{compute_channel(b_ch / CHANNEL_VAL_F, spec_b, brightness)};
+
+  return {0xFF, R, G, B};
+}
+
 void triangle(FrameBuffer &buffer, const Vertex &a, const Vertex &b,
-              const Vertex &c, const Material material,
-              [[maybe_unused]] const Vec3 light_dir, const float bright_a,
-              const float bright_b, const float bright_c, RenderMode mode) {
+              const Vertex &c, const Material material, const Vec3 light_dir,
+              const float bright_a, const float bright_b, const float bright_c,
+              RenderMode mode) {
   float det_val{
       det({a.pos.x, a.pos.y}, {b.pos.x, b.pos.y}, {c.pos.x, c.pos.y})};
   if (std::abs(det_val) < 1e-7) {
@@ -66,81 +161,22 @@ void triangle(FrameBuffer &buffer, const Vertex &a, const Vertex &b,
   }
 
   float inv_det{1.0f / det_val};
-  int min_x{std::clamp(
-      static_cast<int>(std::floor(std::min({a.pos.x, b.pos.x, c.pos.x}))), 0,
-      WIDTH)};
-  int min_y{std::clamp(
-      static_cast<int>(std::floor(std::min({a.pos.y, b.pos.y, c.pos.y}))), 0,
-      HEIGHT)};
-  int max_x{std::clamp(
-      static_cast<int>(std::ceil(std::max({a.pos.x, b.pos.x, c.pos.x}))), 0,
-      WIDTH)};
-  int max_y{std::clamp(
-      static_cast<int>(std::ceil(std::max({a.pos.y, b.pos.y, c.pos.y}))), 0,
-      HEIGHT)};
+  Bounds bounds{calculate_bounds(a, b, c)};
 
-  for (int i{min_x}; i < max_x; i++) {
-    for (int j{min_y}; j < max_y; j++) {
+  for (int i{bounds.min_x}; i < bounds.max_x; i++) {
+    for (int j{bounds.min_y}; j < bounds.max_y; j++) {
       Vertex p{{static_cast<float>(i), static_cast<float>(j)}, {}, {}, {}};
 
-      float l1{det(p.pos, b.pos, c.pos) * inv_det};
-      float l2{det(a.pos, p.pos, c.pos) * inv_det};
-      float l3{det(a.pos, b.pos, p.pos) * inv_det};
-
-      if (l1 < epsilon || l2 < epsilon || l3 < epsilon) {
+      float l1, l2, l3;
+      if (!get_barycentric(p.pos, a.pos, b.pos, c.pos, inv_det, l1, l2, l3)) {
         continue;
       }
 
-      float normal_x{a.normal.x * l1 + b.normal.x * l2 + c.normal.x * l3};
-      float normal_y{a.normal.y * l1 + b.normal.y * l2 + c.normal.y * l3};
-      float normal_z{a.normal.z * l1 + b.normal.z * l2 + c.normal.z * l3};
-      Vec3 normal{normal_x, normal_y, normal_z};
+      float z{interpolate(a.pos.z, b.pos.z, c.pos.z, l1, l2, l3)};
+      p.pos.z = z;
 
-      float brightness{bright_a * l1 + bright_b * l2 + bright_c * l3};
-      float spec_r{}, spec_g{}, spec_b{};
-
-      if (mode == RenderMode::Phong) {
-        brightness = std::clamp(normal.dot(light_dir), ambient, 1.0f);
-        float view_x{a.view_pos.x * l1 + b.view_pos.x * l2 + c.view_pos.x * l3};
-        float view_y{a.view_pos.y * l1 + b.view_pos.y * l2 + c.view_pos.y * l3};
-        float view_z{a.view_pos.z * l1 + b.view_pos.z * l2 + c.view_pos.z * l3};
-        Vec3 view_pos{norm({-view_x, -view_y, -view_z})};
-
-        auto H{norm(light_dir + view_pos)};
-        auto spec{std::pow(std::max(normal.dot(H), 0.0f), material.shine)};
-
-        spec_r = material.specular.r() * spec;
-        spec_g = material.specular.g() * spec;
-        spec_b = material.specular.b() * spec;
-      }
-
-      float r_ch{}, g_ch{}, b_ch{};
-      if (!material.texture || material.texture->pixels.empty()) {
-        r_ch = material.diffuse.r() / 255.0f;
-        g_ch = material.diffuse.g() / 255.0f;
-        b_ch = material.diffuse.b() / 255.0f;
-      } else {
-        auto u{a.uv.x * l1 + b.uv.x * l2 + c.uv.x * l3};
-        auto v{a.uv.y * l1 + b.uv.y * l2 + c.uv.y * l3};
-        auto texel{material.texture->sample(u, v)};
-
-        r_ch = texel.r() / 255.0f;
-        g_ch = texel.g() / 255.0f;
-        b_ch = texel.b() / 255.0f;
-      }
-
-      auto R{std::clamp(
-          static_cast<uint32_t>(ambient + brightness * r_ch * 255.0f + spec_r),
-          0U, 0xFFU)};
-
-      auto G{std::clamp(
-          static_cast<uint32_t>(ambient + brightness * g_ch * 255.0f + spec_g),
-          0U, 0xFFU)};
-      auto B{std::clamp(
-          static_cast<uint32_t>(ambient + brightness * b_ch * 255.0f + spec_b),
-          0U, 0xFFU)};
-
-      Color color{0xFF, R, G, B};
+      Color color{shade_pixel(a, b, c, light_dir, l1, l2, l3, bright_a,
+                              bright_b, bright_c, material, mode)};
       point(buffer, p, color);
     }
   }
