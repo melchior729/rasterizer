57b8c05ce4413fba2d4fb5dec12250da32f88bfa
Abhay Manoj <melchior88729@gmail.com>
2026-06-01 14:15:36 -0400

    fix: changed infinity to max in framebuffer due to floating point optimization flags enabled

--- include/frame_buffer.hpp
+++ include/frame_buffer.hpp
@@ -27,7 +27,7 @@ struct FrameBuffer {
 
   void clear() {
     Color bg{0xFF1E1E1E};
-    depth.fill(std::numeric_limits<float>::infinity());
+    depth.fill(std::numeric_limits<float>::max());
     pixels.fill(bg);
   }
 
