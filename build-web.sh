99ee325f505410e2cf592f6e97e55dd39ef80e14
Abhay Manoj <sanicthecheese@gmail.com>
2026-06-03 11:25:33 -0400

    feat: updated build script and added web/ to gitignore

--- build-web.sh
+++ build-web.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+set -euo pipefail
 
 mkdir -p web
 
-emcc src/main.cpp src/math.cpp src/parser.cpp src/rasterizer.cpp src/scene.cpp src/texture.cpp src/stb_impl.cpp \
+emcc src/main.cpp src/math.cpp src/parser.cpp src/rasterizer.cpp src/scene.cpp \
+  src/texture.cpp src/stb_impl.cpp src/web_api.cpp \
   -Iinclude \
-  -o web/index.html \
+  -o web/rasterizer.js \
   -sUSE_SDL=3 \
   --embed-file models/ \
   --embed-file textures/ \
   -sALLOW_MEMORY_GROWTH=1 \
+  -sEXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' \
   -std=c++20 \
   -O3 \
   -DSDL_MAIN_USE_CALLBACKS=1 \
