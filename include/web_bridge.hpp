9a5d33f7334d6807aded29d613276caac1081ab0
Abhay Manoj <melchior88729@gmail.com>
2026-06-03 22:48:00 -0400

    feat: reaplce day mode with toggle overlay and sens bridge declarations

--- include/web_bridge.hpp
+++ include/web_bridge.hpp
@@ -10,7 +10,29 @@ void web_set_render_mode(int mode);
 
 void web_set_light_angle(float angle);
 
-void web_set_day_mode(int is_day);
+void web_sync_light_angle(float angle);
+
+void web_toggle_overlay();
+
+void web_sync_model_dropdown(int id);
+
+void web_sync_mode_dropdown(int mode);
+
+void web_set_rotate_sens(float v);
+
+void web_set_pan_sens(float v);
+
+void web_set_zoom_sens(float v);
+
+void web_set_light_step(float v);
+
+float web_get_rotate_sens();
+
+float web_get_pan_sens();
+
+float web_get_zoom_sens();
+
+float web_get_light_step();
 
 int web_has_user_mesh();
 
