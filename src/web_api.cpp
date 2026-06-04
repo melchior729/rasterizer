4014197fd1b5dc9a5229bb9e0c7447868f39ab5e
Abhay Manoj <melchior88729@gmail.com>
2026-06-03 22:48:59 -0400

    feat: addeed wasm exports

--- src/web_api.cpp
+++ src/web_api.cpp
@@ -15,9 +15,23 @@ EMSCRIPTEN_KEEPALIVE void set_light_angle(float angle) {
   web_set_light_angle(angle);
 }
 
-EMSCRIPTEN_KEEPALIVE void set_day_mode(int is_day) {
-  web_set_day_mode(is_day);
-}
+EMSCRIPTEN_KEEPALIVE void toggle_overlay() { web_toggle_overlay(); }
+
+EMSCRIPTEN_KEEPALIVE void set_rotate_sens(float v) { web_set_rotate_sens(v); }
+
+EMSCRIPTEN_KEEPALIVE void set_pan_sens(float v) { web_set_pan_sens(v); }
+
+EMSCRIPTEN_KEEPALIVE void set_zoom_sens(float v) { web_set_zoom_sens(v); }
+
+EMSCRIPTEN_KEEPALIVE void set_light_step(float v) { web_set_light_step(v); }
+
+EMSCRIPTEN_KEEPALIVE float get_rotate_sens() { return web_get_rotate_sens(); }
+
+EMSCRIPTEN_KEEPALIVE float get_pan_sens() { return web_get_pan_sens(); }
+
+EMSCRIPTEN_KEEPALIVE float get_zoom_sens() { return web_get_zoom_sens(); }
+
+EMSCRIPTEN_KEEPALIVE float get_light_step() { return web_get_light_step(); }
 
 EMSCRIPTEN_KEEPALIVE int has_user_mesh() { return web_has_user_mesh(); }
 
